b344e281e628677719ce27270f4cbad5669972db
zequ22 <eze22alderete@gmail.com>
2023-10-31 12:38:41 -0300

    Put de Clients

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var express = require('express')
+var bodyParser = require('body-parser') // Para analizar los datos del cuerpo de la solicitud
+var fs = require('fs'); // Módulo File System
 
 var clients = require('./data/clients.json')
 var products = require('./data/products.json')
@@ -7,16 +9,34 @@ var sales = require('./data/sales.json')
 var app = express()
 var port = 3000
 
+app.use(bodyParser.json()) // Middleware para analizar JSON en el cuerpo de la solicitud
+
 app.get('/clients', function(req, res) {
     res.send(clients.list)
-  })
+})
+app.post('/clients', function(req, res) {
+  var newClient = req.body;
+  clients.list.push(newClient);
+  fs.writeFile('./data/clients.json', JSON.stringify(clients, null, 2), (err) => {
+      if (err) {
+          console.error(err);
+          res.status(500).send('Error al guardar los cambios en clients.json');
+      } else {
+          console.log('Cliente agregado y cambios guardados en clients.json');
+          res.status(201).send(newClient);
+      }
+  });
+})
+
 app.get('/products', function(req, res) {
     res.send(products.list)
-  })
+})
+
+
 app.get('/sales', function(req, res) {
     res.send(sales.list)
-  })
+})
 
 app.listen(port, function() {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
